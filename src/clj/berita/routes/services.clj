8697ee732d4f779252ea86ee598458ec4c90b98e
David Liman <limanoit@gmail.com>
2019-04-17 14:29:21 -0700

    comment out swagger path and add .idea to gitignore

--- src/clj/berita/routes/services.clj
+++ src/clj/berita/routes/services.clj
@@ -35,9 +35,9 @@
                  multipart/multipart-middleware]}
 
    ;; swagger documentation
-   ["" {:no-doc true
-        :swagger {:info {:title "my-api"
-                         :description "https://cljdoc.org/d/metosin/reitit"}}}
+   ["" {:no-doc false
+        :swagger {:info {:title "berita (berita lokal API)"
+                         :description "https://github.com/dvliman/berita"}}}
 
     ["/swagger.json"
      {:get (swagger/create-swagger-handler)}]
@@ -47,45 +47,49 @@
              {:url "/api/swagger.json"
               :config {:validator-url nil}})}]]
 
-   ["/ping"
-    {:get (constantly (ok {:message "pong"}))}]
-   
+   ;["/news" {:swagger {:tags ["news"]}}
+   ;
+   ; ["/fetch-news/:category"
+   ;  {:get {:summary "fetch news by category"}}]]
 
-   ["/math"
-    {:swagger {:tags ["math"]}}
+    ;; ["/math"
+    ;; {:swagger {:tags ["math"]}}
 
-    ["/plus"
-     {:get {:summary "plus with spec query parameters"
-            :parameters {:query {:x int?, :y int?}}
-            :responses {200 {:body {:total pos-int?}}}
-            :handler (fn [{{{:keys [x y]} :query} :parameters}]
-                       {:status 200
-                        :body {:total (+ x y)}})}
-      :post {:summary "plus with spec body parameters"
-             :parameters {:body {:x int?, :y int?}}
-             :responses {200 {:body {:total pos-int?}}}
-             :handler (fn [{{{:keys [x y]} :body} :parameters}]
-                        {:status 200
-                         :body {:total (+ x y)}})}}]]
+    ;; ["/plus"
+    ;;  {:get {:summary "plus with spec query parameters"
+    ;;         :parameters {:query {:x int?, :y int?}}
+    ;;         :responses {200 {:body {:total pos-int?}}}
+    ;;         :handler (fn [{{{:keys [x y]} :query} :parameters}]
+    ;;                    {:status 200
+    ;;                     :body {:total (+ x y)}})}
+    ;;   :post {:summary "plus with spec body parameters"
+    ;;          :parameters {:body {:x int?, :y int?}}
+    ;;          :responses {200 {:body {:total pos-int?}}}
+    ;;          :handler (fn [{{{:keys [x y]} :body} :parameters}]
+    ;;                     {:status 200
+    ;;                      :body {:total (+ x y)}})}}]]
 
-   ["/files"
-    {:swagger {:tags ["files"]}}
+   ;; ["/files"
+   ;;  {:swagger {:tags ["files"]}}
 
-    ["/upload"
-     {:post {:summary "upload a file"
-             :parameters {:multipart {:file multipart/temp-file-part}}
-             :responses {200 {:body {:name string?, :size int?}}}
-             :handler (fn [{{{:keys [file]} :multipart} :parameters}]
-                        {:status 200
-                         :body {:name (:filename file)
-                                :size (:size file)}})}}]
+   ;;  ["/upload"
+   ;;   {:post {:summary "upload a file"
+   ;;           :parameters {:multipart {:file multipart/temp-file-part}}
+   ;;           :responses {200 {:body {:name string?, :size int?}}}
+   ;;           :handler (fn [{{{:keys [file]} :multipart} :parameters}]
+   ;;                      {:status 200
+   ;;                       :body {:name (:filename file)
+   ;;                              :size (:size file)}})}}]
 
-    ["/download"
-     {:get {:summary "downloads a file"
-            :swagger {:produces ["image/png"]}
-            :handler (fn [_]
-                       {:status 200
-                        :headers {"Content-Type" "image/png"}
-                        :body (-> "public/img/warning_clojure.png"
-                                  (io/resource)
-                                  (io/input-stream))})}}]]])
+   ;;  ["/download"
+   ;;   {:get {:summary "downloads a file"
+   ;;          :swagger {:produces ["image/png"]}
+   ;;          :handler (fn [_]
+   ;;                     {:status 200
+   ;;                      :headers {"Content-Type" "image/png"}
+   ;;                      :body (-> "public/img/warning_clojure.png"
+   ;;                                (io/resource)
+   ;;                                (io/input-stream))})}}]]
+
+
+   ])
